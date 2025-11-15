aeb1037399c5aea5f650ad465c07f6135a9efc73
Henrique <henriquesanches@zely.pro>
2025-11-14 22:52:04 -0300

    fix: correção no link de logout do Setor de Administração

--- admin-logout.php
+++ admin-logout.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 session_destroy();
-// Redirecionar para a página inicial do site (layout padrão)
-header('Location: /');
+// Redirecionar para a raiz do projeto (evita cair no /dashboard do XAMPP)
+$basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+header('Location: ' . ($basePath === '' ? '/' : $basePath . '/'));
 exit;
 
